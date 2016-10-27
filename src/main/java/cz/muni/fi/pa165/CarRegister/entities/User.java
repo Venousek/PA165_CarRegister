c0dd061042bb405918798c7064ab445c65b6049f
Sevo001 <fero.cernak@gmail.com>
2016-10-27 13:12:16 +0200

    Update User.java

--- src/main/java/cz/muni/fi/pa165/CarRegister/entities/User.java
+++ src/main/java/cz/muni/fi/pa165/CarRegister/entities/User.java
@@ -26,73 +26,73 @@ public class User
 
   //Methods
 
-  public Long getId()
-  {
-    return id;
-  }
+	public Long getId()
+  	{
+    		return id;
+  	}
 		
 	public void setId(Long id)
-  {
+  	{
 		this.id = Id;
 	}
 
 	public String getLogin()
-  {
+  	{
 		return login;
 	}
 
 	public void setLogin(String login)
-  {
+  	{
 		this.login = login;
 	}
 
 	public String getPassword()
-  {
+  	{
 		return password;
 	}
   
 	public void setPassword(String password)
-  {
+  	{
 		this.password = password;
 	}
   
-  public String getFirstname()
-  {
+  	public String getFirstname()
+  	{
 		return firstname;
 	}
   
-  public void setFirstname(String firstname)
-  {
+  	public void setFirstname(String firstname)
+  	{
 		this.firstname = firstname;
 	}
   
-  public String getLastname()
-  {
+ 	public String getLastname()
+	{
 		return lastname;
 	}
   
-  public void setLastname(String lastname)
-  {
+  	public void setLastname(String lastname)
+  	{
 		this.lastname = lastname;
 	}
   
-  public String getEmail()
-  {
+  	public String getEmail()
+  	{
 		return email;
 	}
   
-  public void setEmail(String email)
-  {
+  	public void setEmail(String email)
+  	{
 		this.email = email;
 	}
   
-  public int getRole()
-  {
+  	public int getRole()
+  	{
 		return role;
 	}
   
-  public void setRole(int role)
-  {
+  	public void setRole(int role)
+  	{
 		this.role = role;
 	}
 
